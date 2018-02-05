db1e3515f352aa919e0e0bd55aeb9a04af41f5df
Sledge <SledgeJ@schencksolutions.com>
2018-02-05 10:50:36 -0600

    add app home page with dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppHomeComponent } from './app-home/app-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: AppHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AppHomeComponent } from './app-home/app-home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule,
+  declarations: [AppComponent, AppHomeComponent],
+  imports: [
+    BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule],
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
