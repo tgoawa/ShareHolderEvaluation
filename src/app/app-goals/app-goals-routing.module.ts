b8adffc232b11750bff1accc4c327da0618420c8
Sledge <SledgeJ@schencksolutions.com>
2018-02-06 14:23:18 -0600

    add main and competency goal routing to navigation

--- src/app/app-goals/app-goals-routing.module.ts
+++ src/app/app-goals/app-goals-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GoalsMainComponent } from './goals-main/goals-main.component';
 import { AppGoalsComponent } from './app-goals.component';
+import { GoalsCompetencyComponent } from './goals-competency/goals-competency.component';
 
 const routes: Routes = [
 {
   path: '',
   component: AppGoalsComponent,
   children: [
-    {path: 'main', component: GoalsMainComponent}
+    {path: 'main', component: GoalsMainComponent},
+    {path: 'competency', component: GoalsCompetencyComponent}
   ]
 }
 ];
