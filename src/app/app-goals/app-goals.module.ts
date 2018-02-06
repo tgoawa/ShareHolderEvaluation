11e98733dd7817d06f964194186a53edc4f81503
Sledge <SledgeJ@schencksolutions.com>
2018-02-06 09:06:26 -0600

    create main page card subcomponent

--- src/app/app-goals/app-goals.module.ts
+++ src/app/app-goals/app-goals.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppGoalsRoutingModule } from './app-goals-routing.module';
 import { AppGoalsComponent } from './app-goals.component';
 import { GoalsHeaderComponent } from './goals-header/goals-header.component';
 import { GoalsMainComponent } from './goals-main/goals-main.component';
+import { MainCardComponent } from './goals-main/main-card/main-card.component';
 
 @NgModule({
   imports: [
@@ -15,8 +16,12 @@ import { GoalsMainComponent } from './goals-main/goals-main.component';
     AppGoalsRoutingModule,
     FlexLayoutModule,
     MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
     MatToolbarModule
   ],
-  declarations: [AppGoalsComponent, GoalsHeaderComponent, GoalsMainComponent]
+  declarations: [AppGoalsComponent, GoalsHeaderComponent, GoalsMainComponent, MainCardComponent]
 })
 export class AppGoalsModule { }
---
+++ src/app/app-goals/goals-main/main-card/main-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core/src/change_detection/constants';
+
+@Component({
+  selector: 'app-main-card',
+  templateUrl: './main-card.component.html',
+  styleUrls: ['./main-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainCardComponent implements OnInit {
+  @Input() sectionTitle: string;
+  @Input() goals: string[];
+  @Input() route: string;
+  totalWeight: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
