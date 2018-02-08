bed52e74b196051d937a9c564067757c46e96e9d
Sledge <SledgeJ@schencksolutions.com>
2018-02-08 11:08:16 -0600

    weight data updates independent of each other

--- src/app/app-goals/mock-data/mock-goal-data.ts
+++ src/app/app-goals/mock-data/mock-goal-data.ts
@@ -19,5 +19,26 @@ export const landingPageData = [
         Weight: 10
       }
     ]
+  },
+  {
+    Name: 'WIG',
+    GoalTypeId: 2,
+    Goals: [
+      {
+        Name: 'Test Goal 1',
+        Id: 1,
+        Weight: 10
+      },
+      {
+        Name: 'Test Goal 2',
+        Id: 2,
+        Weight: 10
+      },
+      {
+        Name: 'Test Goal 3',
+        Id: 3,
+        Weight: 10
+      }
+    ]
   }
 ];
