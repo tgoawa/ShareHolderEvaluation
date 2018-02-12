4e099b165c5399896ae88174631c514428f086b8
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-12 10:59:44 -0600

    create base goal model

---
+++ src/app/app-goals/models/goal.ts
@@ -0,0 +1,14 @@
+export class Goal {
+  GoalId: number;
+  GoalType: number;
+  TeamMemberId: number;
+  CompetencyId: number;
+  CompetencyType: number;
+  WIGId: number;
+  IndustryTeamId: number;
+  ServiceLineId: number;
+  GoalName: string;
+  GoalDescription: string;
+  Weight: number;
+  Notes: string;
+}
