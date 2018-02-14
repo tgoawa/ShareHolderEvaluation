c71f4e43f35c7148d16dd22fc426aa5ca33644fd
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-14 09:58:01 -0600

    add formgroup to economic goal component

--- src/app/app-goals/goals-economic/goals-economic.component.ts
+++ src/app/app-goals/goals-economic/goals-economic.component.ts
@@ -15,6 +15,7 @@ const numberMask = createNumberMask({
 })
 export class GoalsEconomicComponent implements OnInit {
   mask = numberMask;
+  economicGoal: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
@@ -23,7 +24,19 @@ export class GoalsEconomicComponent implements OnInit {
 
   private toFormGroup(data: EconomicGoal): FormGroup {
     const formGroup = this.fb.group({
-
+      EconomicGoalId: data.EconomicGoalId,
+      TeamMemberId: data.TeamMemberId,
+      Weight: data.Weight,
+      Year: data.Year,
+      BillingsTiers1_3: data.BillingsTiers1_3,
+      BillingsTiers4_5: data.BillingsTiers4_5,
+      BillingsTier6: data.BillingsTier6,
+      BusinessExistingClients: data.BusinessExistingClients,
+      BusinessNewClients: data.BusinessNewClients,
+      ReferralsSpecialityUnits: data.ReferralsSpecialityUnits,
+      ChargeHours: data.ChargeHours,
+      Realization: data.Realization,
+      DaysinWIP_AR: data.DaysinWIP_AR
     });
 
     return formGroup;
