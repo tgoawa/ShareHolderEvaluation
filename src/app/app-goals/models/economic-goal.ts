b2022b9c24d35d320ef37b68f9632d36427cc19b
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-14 09:37:51 -0600

    create economic goal model

---
+++ src/app/app-goals/models/economic-goal.ts
@@ -0,0 +1,15 @@
+export class EconomicGoal {
+  EconomicGoalId: number;
+  TeamMemberId: number;
+  Weight: number;
+  Year: number;
+  BillingsTiers1_3: number;
+  BillingsTiers4_5: number;
+  BillingsTier6: number;
+  BusinessExistingClients: number;
+  BusinessNewClients: number;
+  ReferralsSpecialityUnits: number;
+  ChargeHours: number;
+  Realization: number;
+  DaysinWIP_AR: number;
+}
