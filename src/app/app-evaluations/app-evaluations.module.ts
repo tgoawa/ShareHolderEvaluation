30689abed45e71729075e00f6c2282ff68d7b15a
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-14 14:17:28 -0600

    create app eval module

---
+++ src/app/app-evaluations/app-evaluations.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppEvaluationsRoutingModule } from './app-evaluations-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AppEvaluationsRoutingModule
+  ],
+  declarations: []
+})
+export class AppEvaluationsModule { }
