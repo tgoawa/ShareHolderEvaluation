a4fb410324d33b54f50d82330e2d0cac98fe2c81
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-15 13:57:32 -0600

    create form group for review notes form

--- src/app/app-goals/goals-review/goals-review.component.ts
+++ src/app/app-goals/goals-review/goals-review.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ReviewModel } from './model/review';
 
 @Component({
   selector: 'app-goals-review',
@@ -14,8 +15,17 @@ export class GoalsReviewComponent implements OnInit {
   ngOnInit() {
   }
 
-  private toFormGroup(data): FormGroup {
+  private toFormGroup(data: ReviewModel): FormGroup {
     const formGroup = this.fb.group({
+      ReviewId: data.ReviewId,
+      TeamMemberId: data.TeamMemberId,
+      ReviewerId: data.ReviewerId,
+      ReviewDate: data.ReviewDate,
+      CompetencyNotes: data.CompetencyNotes,
+      WIGNotes: data.WIGNotes,
+      LeadershipNotes: data.LeadershipNotes,
+      EconomicNotes: data.EconomicNotes,
+      GeneralNotes: data.GeneralNotes
     });
 
     return formGroup;
---
+++ src/app/app-goals/goals-review/model/review.ts
@@ -0,0 +1,11 @@
+export class ReviewModel {
+  ReviewId: number;
+  TeamMemberId: number;
+  ReviewerId: number;
+  ReviewDate: string;
+  CompetencyNotes: string;
+  WIGNotes: string;
+  LeadershipNotes: string;
+  EconomicNotes: string;
+  GeneralNotes: string;
+}
