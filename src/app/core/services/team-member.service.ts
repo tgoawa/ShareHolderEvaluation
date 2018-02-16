9a149cb04ccb3ab93bf5bf66d8f2f9b27a12e5d4
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-16 10:10:23 -0600

    create base for team member service

---
+++ src/app/core/services/team-member.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+
+import { environment } from '../../../environments/environment';
+const api = environment.envApi;
+@Injectable()
+export class TeamMemberService {
+
+}
