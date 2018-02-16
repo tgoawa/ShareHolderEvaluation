8d4105d490c0d6f14a6737b84a45ba32a7787a1f
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-16 10:32:57 -0600

    write get team member function

--- src/app/core/services/team-member.service.ts
+++ src/app/core/services/team-member.service.ts
@@ -10,4 +10,15 @@ const api = environment.envApi;
 @Injectable()
 export class TeamMemberService {
 
+  constructor(private http: Http) { }
+
+  getTeamMember(userName: string) {
+    this.http.get(api + 'EmployeeService/GetEmployee' + userName)
+    .map(response => response.json(), error => console.log('There was an http error'))
+    .subscribe(data => {
+
+    }, error => {
+      console.log('There was an error with the team member data');
+    });
+  }
 }
