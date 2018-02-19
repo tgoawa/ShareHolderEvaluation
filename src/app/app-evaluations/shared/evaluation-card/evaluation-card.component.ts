bce0b7c4361e7a4eaf5becbf5f4ea6336c05c976
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-19 13:14:21 -0600

    add title input to evaluation card

--- src/app/app-evaluations/shared/evaluation-card/evaluation-card.component.ts
+++ src/app/app-evaluations/shared/evaluation-card/evaluation-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-evaluation-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./evaluation-card.component.css']
 })
 export class EvaluationCardComponent implements OnInit {
-
+  @Input() title: string;
   constructor() { }
 
   ngOnInit() {
