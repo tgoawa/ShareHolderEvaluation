c6427c1917d3ad94733d858c1748fb0b88c58e1f
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-27 10:43:03 -0600

    extend goal base component for wig goals view

--- src/app/app-goals/goals-wig/goals-wig.component.ts
+++ src/app/app-goals/goals-wig/goals-wig.component.ts
@@ -1,89 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { GoalData } from '../models/goal';
-import { ActivatedRoute } from '@angular/router';
-import { GoalsService } from '../shared/services/goals.service';
-
-import * as _ from 'lodash';
-import { GoalWeightModel } from '../goals-main/model/weight';
+import { GoalBaseComponent } from '../shared/goal-base/goal-base.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-goals-wig',
   templateUrl: './goals-wig.component.html',
   styleUrls: ['./goals-wig.component.css'],
 })
-export class GoalsWigComponent implements OnInit {
-  goals: GoalData[];
-  goal: GoalData;
+export class GoalsWigComponent extends GoalBaseComponent {
   goalTypeId = 4;
   year = 2017;
-  teamMemberId: number;
-  totalWeight: number;
-  goalWeightData: GoalWeightModel[];
-  constructor(
-    private route: ActivatedRoute,
-    private goalsService: GoalsService
-  ) {}
+  teamMemberId = 1936;
+  // goals: GoalData[];
+  // goal: GoalData;
+  // goalTypeId = 4;
+  // year = 2017;
+  // teamMemberId: number;
+  // totalWeight: number;
+  // goalWeightData: GoalWeightModel[];
+  // constructor(
+  //   private route: ActivatedRoute,
+  //   private goalsService: GoalsService
+  // ) {}
 
-  ngOnInit() {
-    this.teamMemberId = 1936;
-    this.getGoals(this.teamMemberId, this.goalTypeId, this.year);
-  }
+  // ngOnInit() {
+  //   this.teamMemberId = 1936;
+  //   this.getGoals(this.teamMemberId, this.goalTypeId, this.year);
+  // }
 
-  onSetExistingGoal(goal: GoalData) {
-    this.goal = undefined;
-    this.goal = _.cloneDeep(goal);
-  }
+  // onSetExistingGoal(goal: GoalData) {
+  //   this.goal = undefined;
+  //   this.goal = _.cloneDeep(goal);
+  // }
 
-  createGoalWeightData(goalData: GoalData[]): GoalWeightModel[] {
-    const dataDictionary: GoalWeightModel[] = [];
-    for (let x = 0; x < goalData.length; x++) {
-      const goalWeightModel = new GoalWeightModel(goalData[x].GoalId, goalData[x].Weight);
-      dataDictionary.push(goalWeightModel);
-    }
-    return dataDictionary;
-  }
+  // createGoalWeightData(goalData: GoalData[]): GoalWeightModel[] {
+  //   const dataDictionary: GoalWeightModel[] = [];
+  //   for (let x = 0; x < goalData.length; x++) {
+  //     const goalWeightModel = new GoalWeightModel(goalData[x].GoalId, goalData[x].Weight);
+  //     dataDictionary.push(goalWeightModel);
+  //   }
+  //   return dataDictionary;
+  // }
 
-  onWeightChange(goalWeightModel: GoalWeightModel) {
-    const updatedDataDictionary: GoalWeightModel[] = [];
-    for (let x = 0; x < this.goalWeightData.length; x ++) {
-      if (goalWeightModel.GoalId === this.goalWeightData[x].GoalId) {
-        updatedDataDictionary.push(goalWeightModel);
-      } else {
-        updatedDataDictionary.push(this.goalWeightData[x]);
-      }
-    }
-    this.goalWeightData = updatedDataDictionary;
-  }
+  // onWeightChange(goalWeightModel: GoalWeightModel) {
+  //   const updatedDataDictionary: GoalWeightModel[] = [];
+  //   for (let x = 0; x < this.goalWeightData.length; x ++) {
+  //     if (goalWeightModel.GoalId === this.goalWeightData[x].GoalId) {
+  //       updatedDataDictionary.push(goalWeightModel);
+  //     } else {
+  //       updatedDataDictionary.push(this.goalWeightData[x]);
+  //     }
+  //   }
+  //   this.goalWeightData = updatedDataDictionary;
+  // }
 
-  private getGoals(id: number, goalTypeId: number, year: number) {
-    this.goalsService.getGoals(id, goalTypeId, year).subscribe(
-      data => {
-        this.goals = data;
-        this.goalWeightData = this.createGoalWeightData(this.goals);
-        this.setGoal(this.goals, id);
-      },
-      error => {
-        console.log('Could not retrieve list of goals');
-      }
-    );
-  }
+  // private getGoals(id: number, goalTypeId: number, year: number) {
+  //   this.goalsService.getGoals(id, goalTypeId, year).subscribe(
+  //     data => {
+  //       this.goals = data;
+  //       this.goalWeightData = this.createGoalWeightData(this.goals);
+  //       this.setGoal(this.goals, id);
+  //     },
+  //     error => {
+  //       console.log('Could not retrieve list of goals');
+  //     }
+  //   );
+  // }
 
-  private setGoal(goals: GoalData[], teamMemberId: number) {
-    this.route.params.subscribe(params => {
-      if (params['id'] === '0') {
-        this.goal = new GoalData(this.goalTypeId, teamMemberId);
-      } else {
-        this.setGoalFromRoute(goals, params['id']);
-      }
-    });
-  }
+  // private setGoal(goals: GoalData[], teamMemberId: number) {
+  //   this.route.params.subscribe(params => {
+  //     if (params['id'] === '0') {
+  //       this.goal = new GoalData(this.goalTypeId, teamMemberId);
+  //     } else {
+  //       this.setGoalFromRoute(goals, params['id']);
+  //     }
+  //   });
+  // }
 
-  private setGoalFromRoute(goals: GoalData[], goalId: number) {
-    for (let x = 0; x < goals.length; x++) {
-      if (+goalId === goals[x].GoalId) {
-        this.onSetExistingGoal(goals[x]);
-        break;
-      }
-    }
-  }
+  // private setGoalFromRoute(goals: GoalData[], goalId: number) {
+  //   for (let x = 0; x < goals.length; x++) {
+  //     if (+goalId === goals[x].GoalId) {
+  //       this.onSetExistingGoal(goals[x]);
+  //       break;
+  //     }
+  //   }
+  // }
 }
