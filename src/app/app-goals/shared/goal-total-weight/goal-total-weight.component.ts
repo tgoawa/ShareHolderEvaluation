b832db439dc221c2cecf0dbc226e59ff776b06d4
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-27 08:09:07 -0600

    create goal total weight component basic functionality

--- src/app/app-goals/shared/goal-total-weight/goal-total-weight.component.ts
+++ src/app/app-goals/shared/goal-total-weight/goal-total-weight.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { Goal } from '../../goals-main/model/goals';
+import { GoalData } from '../../models/goal';
+import { GoalWeightModel } from '../../goals-main/model/weight';
 
 @Component({
   selector: 'app-goal-total-weight',
@@ -7,35 +8,34 @@ import { Goal } from '../../goals-main/model/goals';
   styleUrls: ['./goal-total-weight.component.css']
 })
 export class GoalTotalWeightComponent implements OnInit, OnChanges {
-  @Input() goalListWeights: Goal[];
+  @Input() goalData: GoalData[];
   totalWeight: number;
-  weightDataDictionary: Goal[];
+  weightDataDictionary: GoalWeightModel[];
   constructor() { }
 
   ngOnInit() {
-    this.weightDataDictionary = this.createWeightDataDictionary(this.goalListWeights);
-    this.totalWeight = this.calculateTotalWeight(this.goalListWeights);
+    this.weightDataDictionary = this.createWeightDataDictionary(this.goalData);
+    this.totalWeight = this.calculateTotalWeight(this.weightDataDictionary);
   }
 
   ngOnChanges() {
-    this.totalWeight = this.calculateTotalWeight(this.goalListWeights);
+    this.weightDataDictionary = this.createWeightDataDictionary(this.goalData);
+    this.totalWeight = this.calculateTotalWeight(this.weightDataDictionary);
   }
 
-  calculateTotalWeight(goals: Goal[]): number {
+  calculateTotalWeight(goals: GoalWeightModel[]): number {
     let calculatedWeight = 0;
     for (let x = 0; x < goals.length; x++) {
-      calculatedWeight = calculatedWeight + goals[x].Weight;
+      calculatedWeight = calculatedWeight + goals[x].GoalWeight;
     }
     return calculatedWeight;
   }
 
-    createWeightDataDictionary(goalListWeights: Goal[]) {
+  createWeightDataDictionary(goalData: GoalData[]) {
     const dataDictionary = [];
-    for (let x = 0; x < goalListWeights.length; x++) {
-      const goalWeightData = new Goal();
-      goalWeightData.Id = goalListWeights[x].Id;
-      goalWeightData.Weight = goalListWeights[x].Weight;
-      dataDictionary.push(goalWeightData);
+    for (let x = 0; x < goalData.length; x++) {
+      const goalWeightModel = new GoalWeightModel(goalData[x].GoalId, goalData[x].Weight);
+      dataDictionary.push(goalWeightModel);
     }
     return dataDictionary;
   }
