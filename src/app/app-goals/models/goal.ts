e964f5dde5c0b362055dcc1a9d529457f6ec7bdf
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-28 13:37:56 -0600

    changed goal model to default id's to 0

--- src/app/app-goals/models/goal.ts
+++ src/app/app-goals/models/goal.ts
@@ -20,11 +20,11 @@ export class GoalData {
     this.GoalId = 0;
     this.GoalTypeId = goalTypeId;
     this.TeamMemberId = teamMemberId;
-    this.GoalCompetencyId = null;
-    this.GoalCompetencyTypeId = null;
-    this.GoalWIGId = null;
-    this.IndustryTeamId = null;
-    this.ServiceLineId = null;
+    this.GoalCompetencyId = 0;
+    this.GoalCompetencyTypeId = 0;
+    this.GoalWIGId = 0;
+    this.IndustryTeamId = 0;
+    this.ServiceLineId = 0;
     this.Name = '';
     this.GoalDescription = '';
     this.Weight = 5;
