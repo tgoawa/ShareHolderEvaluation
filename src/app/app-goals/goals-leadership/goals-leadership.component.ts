fc8a8dd829b9929fc229f43429a6f5c82a158ab4
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-02-28 12:53:04 -0600

    added leadership goal to dashboard model

--- src/app/app-goals/goals-leadership/goals-leadership.component.ts
+++ src/app/app-goals/goals-leadership/goals-leadership.component.ts
@@ -1,32 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { GoalData } from '../models/goal';
-import { ActivatedRoute } from '@angular/router';
+import { Component} from '@angular/core';
+import { GoalBaseComponent } from '../shared/goal-base/goal-base.component';
+
 
 @Component({
   selector: 'app-goals-leadership',
   templateUrl: './goals-leadership.component.html',
   styleUrls: ['./goals-leadership.component.css']
 })
-export class GoalsLeadershipComponent implements OnInit {
-  goal: GoalData;
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.setGoal(3, 1936);
-  }
-
-  private setGoal(goalTypeId: number, teamMemberId: number) {
-    this.route.params.subscribe(params => {
-      if (params['id'] === '0') {
-        this.goal = new GoalData(goalTypeId, teamMemberId);
-      } else {
-        this.goal = this.getGoal(params['id']);
-      }
-    });
-  }
-
-  private getGoal(id: number): GoalData {
-    // logic to get goal based on id passed through route
-    return;
-  }
+export class GoalsLeadershipComponent extends GoalBaseComponent {
+  goalTypeId = 6;
 }
