c0e878f9bb3dcd64020f7c1aa1fa2c7acc5a86bc
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-05 14:19:20 -0600

    fix data bind issues for economic goals

--- src/app/app-goals/goals-economic/goals-economic.component.ts
+++ src/app/app-goals/goals-economic/goals-economic.component.ts
@@ -32,7 +32,6 @@ export class GoalsEconomicComponent implements OnInit {
     this.currentYearHeading = this.year.toString() + this.currentYearHeading;
     this.previousYearActualHeading = this.previousYearActualHeading + this.previousYear.toString();
     this.previousYearGoalheading = this.previousYear.toString() + this.previousYearGoalheading;
-    this.economicGoalForm = this.toFormGroup(new EconomicGoal());
     this.getEconomicGoal();
   }
 
@@ -40,6 +39,7 @@ export class GoalsEconomicComponent implements OnInit {
     this.goalService.getEconomicGoals(this.teamMemberId, this.year)
     .subscribe(data => {
       this.economicGoal = data;
+      this.economicGoalForm = this.toFormGroup(this.economicGoal.CurrentEconomicGoal);
     }, error => {
       console.log(error);
     });
@@ -51,22 +51,18 @@ export class GoalsEconomicComponent implements OnInit {
       TeamMemberId: data.TeamMemberId,
       Weight: data.Weight,
       Year: data.Year,
-      BillingsTiers1_3: 0,
-      BillingsTiers4_5: 0,
-      BillingsTier6: 0,
-      BusinessNewClients: 0,
-      BusinessExistingClients: 0,
-      ReferralsSpecialityUnits: 0,
-      ChargeHours: 0,
-      Realization: 0,
-      DaysinWIP_AR: 0
+      BillingsTiers1_3: data.Detail.BillingsTiers1_3,
+      BillingsTiers4_5: data.Detail.BillingsTiers4_5,
+      BillingsTier6: data.Detail.BillingsTier6,
+      BusinessNewClients: data.Detail.BusinessNewClients,
+      BusinessExistingClients: data.Detail.BusinessExistingClients,
+      ReferralsSpecialityUnits: data.Detail.ReferralsSpecialityUnits,
+      ChargeHours: data.Detail.ChargeHours,
+      Realization: data.Detail.Realization,
+      DaysinWIP_AR: data.Detail.DaysinWIP_AR
     });
 
     return formGroup;
-  }
-
-  private mapEconomicGoalData(data: EconomicGoal[]) {
-
   }
 
 }
