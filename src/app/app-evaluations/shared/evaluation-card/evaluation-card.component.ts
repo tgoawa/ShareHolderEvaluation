258daa3a5dab696e2592eb036e5ff892822b7c7d
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-16 09:57:26 -0500

    added totals to eval form card

--- src/app/app-evaluations/shared/evaluation-card/evaluation-card.component.ts
+++ src/app/app-evaluations/shared/evaluation-card/evaluation-card.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EvaluationCardComponent implements OnInit {
   @Input() title: string;
+  scores = [
+    10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+  ];
+  currentScore = 10;
   constructor() { }
 
   ngOnInit() {
