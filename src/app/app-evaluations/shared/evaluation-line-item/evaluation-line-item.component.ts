d991faa151cac74d7cf0753fe1725738044bb47f
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-20 14:30:23 -0500

    create eventemitter for score types in eval line item

--- src/app/app-evaluations/shared/evaluation-line-item/evaluation-line-item.component.ts
+++ src/app/app-evaluations/shared/evaluation-line-item/evaluation-line-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-evaluation-line-item',
@@ -9,11 +9,24 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core
 export class EvaluationLineItemComponent implements OnInit {
   @Input() goalName: string;
   @Input() goalWeight: string;
+  @Output() selfScore: EventEmitter<number> = new EventEmitter<number>();
+  @Output() picScore: EventEmitter<number> = new EventEmitter<number>();
+  @Output() committeeScore: EventEmitter<number> = new EventEmitter<number>();
 
   ratings = [ 10, 9, 8, 7, 6, 5, 4, 3, 2 , 1];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelfScoreChange(val: number) {
+    this.selfScore.emit(val);
+  }
+  onPicScoreChange(val: number) {
+    this.picScore.emit(val);
+  }
+  onCommitteeScoreChange(val: number) {
+    this.committeeScore.emit(val);
   }
 
 }
