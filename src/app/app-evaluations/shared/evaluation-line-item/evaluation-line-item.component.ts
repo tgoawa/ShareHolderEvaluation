6ab757c620b797a705242954da0c93ab8509b64b
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-21 10:48:14 -0500

    bind mock eval item data to component

--- src/app/app-evaluations/shared/evaluation-line-item/evaluation-line-item.component.ts
+++ src/app/app-evaluations/shared/evaluation-line-item/evaluation-line-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { EvaluationItem } from '../models/Evaluation';
 
 @Component({
   selector: 'app-evaluation-line-item',
@@ -7,16 +8,30 @@ import { Component, OnInit, Input, ChangeDetectionStrategy, Output, EventEmitter
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EvaluationLineItemComponent implements OnInit {
-  @Input() goalName: string;
-  @Input() goalWeight: string;
+  @Input() evalItem: EvaluationItem;
   @Output() selfScore: EventEmitter<number> = new EventEmitter<number>();
   @Output() picScore: EventEmitter<number> = new EventEmitter<number>();
   @Output() committeeScore: EventEmitter<number> = new EventEmitter<number>();
+
+  itemId: number;
+  itemName: string;
+  comments: string;
+  weight: number;
+  itemSelfScore: number;
+  itemPicScore: number;
+  itemCommitteeScore: number;
 
   ratings = [ 10, 9, 8, 7, 6, 5, 4, 3, 2 , 1];
   constructor() { }
 
   ngOnInit() {
+    this.itemId = this.evalItem.ItemId;
+    this.itemName = this.evalItem.ItemName;
+    this.comments = this.evalItem.Comments;
+    this.weight = this.evalItem.Weight;
+    this.itemSelfScore = this.evalItem.SelfScore;
+    this.itemPicScore = this.evalItem.PICScore;
+    this.itemCommitteeScore = this.evalItem.CommitteeScore;
   }
 
   onSelfScoreChange(val: number) {
