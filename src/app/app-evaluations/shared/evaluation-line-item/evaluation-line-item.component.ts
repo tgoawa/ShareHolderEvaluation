330321fb997c38fdbf1410c19cf3640410dd970b
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-21 11:18:25 -0500

    create event emitters for eval item score values

--- src/app/app-evaluations/shared/evaluation-line-item/evaluation-line-item.component.ts
+++ src/app/app-evaluations/shared/evaluation-line-item/evaluation-line-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 import { EvaluationItem } from '../models/Evaluation';
+import { ScoreDictionary } from '../models/score-dictionary';
 
 @Component({
   selector: 'app-evaluation-line-item',
@@ -9,9 +10,9 @@ import { EvaluationItem } from '../models/Evaluation';
 })
 export class EvaluationLineItemComponent implements OnInit {
   @Input() evalItem: EvaluationItem;
-  @Output() selfScore: EventEmitter<number> = new EventEmitter<number>();
-  @Output() picScore: EventEmitter<number> = new EventEmitter<number>();
-  @Output() committeeScore: EventEmitter<number> = new EventEmitter<number>();
+  @Output() selfScore: EventEmitter<ScoreDictionary> = new EventEmitter<ScoreDictionary>();
+  @Output() picScore: EventEmitter<ScoreDictionary> = new EventEmitter<ScoreDictionary>();
+  @Output() committeeScore: EventEmitter<ScoreDictionary> = new EventEmitter<ScoreDictionary>();
 
   itemId: number;
   itemName: string;
@@ -34,14 +35,23 @@ export class EvaluationLineItemComponent implements OnInit {
     this.itemCommitteeScore = this.evalItem.CommitteeScore;
   }
 
-  onSelfScoreChange(val: number) {
-    this.selfScore.emit(val);
+  onSelfScoreChange() {
+    const outputData = new ScoreDictionary();
+    outputData.id = this.itemId;
+    outputData.value = this.itemSelfScore;
+    this.selfScore.emit(outputData);
   }
-  onPicScoreChange(val: number) {
-    this.picScore.emit(val);
+  onPicScoreChange() {
+    const outputData = new ScoreDictionary();
+    outputData.id = this.itemId;
+    outputData.value = this.itemPicScore;
+    this.picScore.emit(outputData);
   }
-  onCommitteeScoreChange(val: number) {
-    this.committeeScore.emit(val);
+  onCommitteeScoreChange() {
+    const outputData = new ScoreDictionary();
+    outputData.id = this.itemId;
+    outputData.value = this.itemCommitteeScore;
+    this.committeeScore.emit(outputData);
   }
 
 }
