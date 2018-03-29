eab9af14e40bc7f7e63813e5d18ea9578d9fb333
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-29 11:27:44 -0500

    Remove final score input from evaluation page

--- src/app/app-evaluations/shared/evaluation-card/evaluation-card.component.ts
+++ src/app/app-evaluations/shared/evaluation-card/evaluation-card.component.ts
@@ -12,7 +12,7 @@ export class EvaluationCardComponent implements OnInit, OnChanges {
   @Input() title: string;
   @Input() evaluation: EvaluationData;
   scores = [
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+    10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
   ];
 
   averageSelfScore: number;
