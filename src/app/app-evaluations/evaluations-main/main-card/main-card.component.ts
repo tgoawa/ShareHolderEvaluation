e03f5862ba69200e6ed4e3968b12bc9196c4ffcd
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-03-30 09:45:32 -0500

    update card to reflect new changes (Not implemented yet)

--- src/app/app-evaluations/evaluations-main/main-card/main-card.component.ts
+++ src/app/app-evaluations/evaluations-main/main-card/main-card.component.ts
@@ -12,39 +12,21 @@ export class MainCardComponent implements OnInit, AfterViewInit {
   @Input() route: string;
   @Output() evalScoreData: EventEmitter<ScoreData> = new EventEmitter<ScoreData>();
   outputData: ScoreData;
-  totalScore: number;
   routeName: string;
 
   constructor() { }
 
   ngOnInit() {
     this.routeName = this.data.Name.toLocaleLowerCase();
-    this.totalScore = this.calculateTotalScore();
   }
 
   ngAfterViewInit() {
-    this.sendScoreData();
+    // this.sendScoreData();
   }
 
-  calculateTotalScore() {
-    const data = this.data.Evaluations;
-    let value = 0;
-    for (let x = 0; x < data.length; x++) {
-      if (data[x].Score !== null) {
-        value = value + data[x].Score;
-      }
-    }
-    return value;
-  }
-
-  updateTotalScore() {
-    this.totalScore = this.calculateTotalScore();
-    this.sendScoreData();
-  }
-
-  sendScoreData() {
-    this.outputData = new ScoreData(this.data.EvaluationTypeId, this.totalScore);
-    this.evalScoreData.emit(this.outputData);
-  }
+  // sendScoreData() {
+  //   this.outputData = new ScoreData(this.data.EvaluationTypeId, this.totalScore);
+  //   this.evalScoreData.emit(this.outputData);
+  // }
 
 }
