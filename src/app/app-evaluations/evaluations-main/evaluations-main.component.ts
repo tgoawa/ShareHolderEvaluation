44c9a845c46cd2db21dccb5db6c73b4a33687bdd
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-05-08 14:46:17 -0500

    remove unused code

--- src/app/app-evaluations/evaluations-main/evaluations-main.component.ts
+++ src/app/app-evaluations/evaluations-main/evaluations-main.component.ts
@@ -31,30 +31,4 @@ export class EvaluationsMainComponent implements OnInit {
     });
   }
 
-  // createScoreDataDictionary() {
-  //   const dataDictionary = [];
-  //   for (let x = 0; x < this.evaluations.length; x++) {
-  //     this.evalScoreData = new ScoreData(this.evaluations[x].EvaluationTypeId, 0);
-  //     dataDictionary.push(this.evalScoreData);
-  //   }
-  //   return dataDictionary;
-  // }
-
-  // assignScoreTotal(evalData: ScoreData) {
-  //   for (let x = 0; x < this.scoreDataDictionary.length; x++) {
-  //     if (evalData.Id === this.scoreDataDictionary[x].Id) {
-  //       this.scoreDataDictionary[x].Score = evalData.Score;
-  //     }
-  //   }
-  //   this.totalScore = this.calculateTotalScore();
-  // }
-
-  // calculateTotalScore() {
-  //   let calculateScore = 0;
-  //   for (let x = 0; x < this.scoreDataDictionary.length; x++) {
-  //     calculateScore = calculateScore + this.scoreDataDictionary[x].Score;
-  //   }
-  //   return calculateScore;
-  // }
-
 }
