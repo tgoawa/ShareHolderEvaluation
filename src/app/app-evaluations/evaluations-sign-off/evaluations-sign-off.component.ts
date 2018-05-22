a50a363217aeb09ea51c8fed76b1b7274904b923
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-05-22 13:28:41 -0500

    add ui for pic/consensus sign off forms

--- src/app/app-evaluations/evaluations-sign-off/evaluations-sign-off.component.ts
+++ src/app/app-evaluations/evaluations-sign-off/evaluations-sign-off.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EvaluationService } from '../shared/services/evaluation.service';
 import { EvaluationModel } from '../shared/models/Evaluation';
+import { LoginService } from '../../core/services/login.service';
 
+import * as CryptoJS from 'crypto-js';
+import { User } from '../../login/model/user';
 @Component({
   selector: 'app-evaluations-sign-off',
   templateUrl: './evaluations-sign-off.component.html',
@@ -14,13 +17,17 @@ export class EvaluationsSignOffComponent implements OnInit {
   picSignOff: FormGroup;
   consensusSignOff: FormGroup;
 
-  constructor(private fb: FormBuilder, private evaluationService: EvaluationService) { }
+  constructor(private fb: FormBuilder,
+    private evaluationService: EvaluationService,
+    private loginService: LoginService) { }
 
   ngOnInit() {
     this.evaluationService.evaluationModel$.subscribe(data => {
       if (data) {
         this.evaluationData = data;
         this.createShareholderForm(this.evaluationData);
+        this.picSignOff = this.toFormGroup();
+        this.consensusSignOff = this.toFormGroup();
       }
     });
   }
@@ -31,21 +38,22 @@ export class EvaluationsSignOffComponent implements OnInit {
     });
   }
 
-  private toFormGroup() {
-    const formGroup = this.fb.group({
-      username: [null, [Validators.required, Validators.minLength(4)]],
-      password: [null, [Validators.required, Validators.minLength(8)]]
-    });
-
-    return formGroup;
-  }
-
   onConsensusSignOff() {
     //
   }
 
-  onPicSignOff() {
-    //
+  onPicSignOff(form: FormGroup) {
+    const encUser = this.encryptUser(form);
+    if (this.checkUser(encUser)) {
+      this.evaluationService.updatePicSignOff(this.evaluationData)
+      .subscribe(data => {
+        if (data) {
+          this.evaluationData.IsPICSignOff = true;
+        }
+      }, error => {
+        console.error(error);
+      });
+    }
   }
 
   onShareholderSignOff() {
@@ -57,6 +65,39 @@ export class EvaluationsSignOffComponent implements OnInit {
     }, error => {
       console.error(error);
     });
+  }
+
+  private checkUser(user: User) {
+    this.loginService.isValid(user)
+      .subscribe(data => {
+        if (data) {
+          return data;
+        }
+      }, error => {
+        console.error(error);
+      });
+  }
+
+  private encryptUser(form: FormGroup) {
+    const key = CryptoJS.enc.Utf8.parse('8080808080808080');
+    const iv = CryptoJS.enc.Utf8.parse('8080808080808080');
+    const encryptedUser: User = {
+      username: null,
+      password: null
+    };
+
+    encryptedUser.username = CryptoJS.AES.encrypt(form.get('username').value, key, { iv: iv }).toString();
+    encryptedUser.password = CryptoJS.AES.encrypt(form.get('password').value, key, { iv: iv }).toString();
+    return encryptedUser;
+  }
+
+  private toFormGroup() {
+    const formGroup = this.fb.group({
+      username: [null, [Validators.required, Validators.minLength(4)]],
+      password: [null, [Validators.required, Validators.minLength(8)]]
+    });
+
+    return formGroup;
   }
 
 }
