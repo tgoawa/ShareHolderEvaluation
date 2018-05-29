5d55101ea26fab10c360c043b599fecff1f19b76
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-05-29 09:23:15 -0500

    refactor evaluation data calls to get based on team member signed in.

--- src/app/app-evaluations/evaluations-main/evaluations-main.component.ts
+++ src/app/app-evaluations/evaluations-main/evaluations-main.component.ts
@@ -4,6 +4,8 @@ import { EvaluationService } from '../shared/services/evaluation.service';
 import { EvaluationModel } from '../shared/models/Evaluation';
 import { Observable } from 'rxjs/Observable';
 import { PowerLevel } from '../shared/models/powerLevel';
+import { TeamMemberService } from '../../core/services/team-member.service';
+import { TeamMember } from '../../core/model/team-member';
 
 @Component({
   selector: 'app-evaluations-main',
@@ -13,17 +15,16 @@ import { PowerLevel } from '../shared/models/powerLevel';
 export class EvaluationsMainComponent implements OnInit, OnChanges {
   evaluationData: EvaluationModel;
   powerLevelDropdown: PowerLevel;
+  teamMember: TeamMember;
   teamMemberId = 1936;
   year: number;
   constructor(private yearService: YearSelectionService,
+    private teamMemberService: TeamMemberService,
     private evaluationService: EvaluationService) { }
 
   ngOnInit() {
     this.getPowerLevelDropdown();
-    this.yearService.selectedEvalYear$.subscribe(data => {
-      this.year = data;
-      this.evaluationService.getEvaluationModel(this.teamMemberId, this.year);
-    });
+    this.getData();
     this.evaluationService.evaluationModel$.subscribe(data => {
       this.evaluationData = data;
     }, error => console.error(error));
@@ -31,10 +32,7 @@ export class EvaluationsMainComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     this.getPowerLevelDropdown();
-    this.yearService.selectedEvalYear$.subscribe(data => {
-      this.year = data;
-      this.evaluationService.getEvaluationModel(this.teamMemberId, this.year);
-    });
+    this.getData();
     this.evaluationService.evaluationModel$.subscribe(data => {
       this.evaluationData = data;
     }, error => console.error(error));
@@ -47,6 +45,21 @@ export class EvaluationsMainComponent implements OnInit, OnChanges {
     }, error => {
       console.error(error);
     });
+  }
+
+  private getData() {
+    this.teamMemberService.teamMember$
+      .subscribe(teamMemberObject => {
+        this.teamMember = teamMemberObject;
+        this.yearService.selectedEvalYear$.subscribe(data => {
+          this.year = data;
+          this.evaluationService.getEvaluationModel(this.teamMember.TeamMemberId, this.year);
+        }, error => {
+          console.error(error);
+        });
+      }, error => {
+        console.error('Could not retrieve team member data!');
+      });
   }
 
 }
