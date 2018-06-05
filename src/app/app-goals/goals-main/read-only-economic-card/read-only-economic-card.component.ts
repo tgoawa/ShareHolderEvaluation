2e2f772231c5aa32fe56b489a72be6b235486b38
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-06-05 08:08:12 -0500

    add read-only economic main card

--- src/app/app-goals/goals-main/read-only-economic-card/read-only-economic-card.component.ts
+++ src/app/app-goals/goals-main/read-only-economic-card/read-only-economic-card.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { EconomicCardComponent } from '../economic-card/economic-card.component';
 
 @Component({
   selector: 'app-read-only-economic-card',
   templateUrl: './read-only-economic-card.component.html',
   styleUrls: ['./read-only-economic-card.component.css']
 })
-export class ReadOnlyEconomicCardComponent implements OnInit {
+export class ReadOnlyEconomicCardComponent extends EconomicCardComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
