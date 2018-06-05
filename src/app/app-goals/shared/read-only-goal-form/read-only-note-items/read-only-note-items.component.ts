985ce9504fbe749fddc949351e73b243fd9b8512
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-06-05 08:45:07 -0500

    create read only note sub components

--- src/app/app-goals/shared/read-only-goal-form/read-only-note-items/read-only-note-items.component.ts
+++ src/app/app-goals/shared/read-only-goal-form/read-only-note-items/read-only-note-items.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteItemsComponent } from '../../goal-form-card/note-items/note-items.component';
 
 @Component({
   selector: 'app-read-only-note-items',
   templateUrl: './read-only-note-items.component.html',
   styleUrls: ['./read-only-note-items.component.css']
 })
-export class ReadOnlyNoteItemsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ReadOnlyNoteItemsComponent extends NoteItemsComponent {
 
 }
--- src/app/app-goals/shared/read-only-goal-form/read-only-note-list/read-only-note-list.component.ts
+++ src/app/app-goals/shared/read-only-goal-form/read-only-note-list/read-only-note-list.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesListComponent } from '../../goal-form-card/notes-list/notes-list.component';
 
 @Component({
   selector: 'app-read-only-note-list',
   templateUrl: './read-only-note-list.component.html',
   styleUrls: ['./read-only-note-list.component.css']
 })
-export class ReadOnlyNoteListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ReadOnlyNoteListComponent extends NotesListComponent {
 
 }
