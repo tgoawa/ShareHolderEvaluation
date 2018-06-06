c665b78f9f3c457f36c644c8b1171c8cd5bb43f2
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-06-06 10:22:48 -0500

    fix broken links in goals dashboard

--- src/app/app-goals/goals-review/goals-review.component.ts
+++ src/app/app-goals/goals-review/goals-review.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ReviewModel } from './model/review';
+import { YearSelectionService } from '../../core/services/year-selection.service';
+import { TeamMemberService } from '../../core/services/team-member.service';
+import { TeamMember } from '../../core/model/team-member';
+import { GoalsService } from '../shared/services/goals.service';
 
 @Component({
   selector: 'app-goals-review',
@@ -9,12 +13,17 @@ import { ReviewModel } from './model/review';
 })
 export class GoalsReviewComponent implements OnInit {
   reviewForm: FormGroup;
-  data = new ReviewModel();
+  teamMember: TeamMember;
+  year: number;
+  reviewModel: ReviewModel;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private goalService: GoalsService,
+    private yearService: YearSelectionService,
+    private teamMemberService: TeamMemberService) { }
 
   ngOnInit() {
-    this.reviewForm = this.toFormGroup(this.data);
+    this.getData();
   }
 
   private toFormGroup(data: ReviewModel): FormGroup {
@@ -27,10 +36,50 @@ export class GoalsReviewComponent implements OnInit {
       WIGNote: data.WIGNote,
       LeadershipNote: data.LeadershipNote,
       EconomicNote: data.EconomicNote,
-      GeneralNote: data.GeneralNote
+      GeneralNote: data.GeneralNote,
+      Year: data.Year
     });
 
     return formGroup;
+  }
+
+  private getData() {
+    this.teamMemberService.teamMember$.subscribe(data => {
+      this.teamMember = data;
+      this.yearService.selectedGoalYear$.subscribe(val => {
+        this.year = val;
+        this.getReview(this.teamMember.TeamMemberId, this.year);
+      }, error => console.error(error));
+    });
+  }
+
+  private getReview(teamMemberId: number, year: number) {
+    this.goalService.getShareholderReview(teamMemberId, year)
+      .subscribe(data => {
+        if (data !== null) {
+          this.reviewModel = data;
+        } else {
+          this.setReviewObject();
+        }
+      }, error => {
+        console.error(error);
+      });
+  }
+
+  private setReviewObject() {
+    this.reviewModel = new ReviewModel();
+    this.reviewModel.CompetencyNote = '';
+    this.reviewModel.CreatedDate = '';
+    this.reviewModel.EconomicNote = '';
+    this.reviewModel.GeneralNote = '';
+    this.reviewModel.LeadershipNote = '';
+    this.reviewModel.ReviewerId = undefined;
+    this.reviewModel.ReviewId = 0;
+    this.reviewModel.TeamMemberId = this.teamMember.TeamMemberId;
+    this.reviewModel.WIGNote = '';
+    this.reviewModel.Year = this.year;
+
+    this.reviewForm = this.toFormGroup(this.reviewModel);
   }
 
 }
