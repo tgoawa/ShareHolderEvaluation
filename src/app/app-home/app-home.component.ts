3d66c881029a128577a3c3661aab767b9ac3c6cd
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-06-25 11:15:04 -0500

    prep for cookie functionality

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TeamMember } from '../core/model/team-member';
 import { TeamMemberService } from '../core/services/team-member.service';
 
+import { Cookie } from 'ng2-cookies';
+
 @Component({
   selector: 'app-app-home',
   templateUrl: './app-home.component.html',
@@ -13,6 +15,11 @@ export class AppHomeComponent implements OnInit {
 
   ngOnInit() {
     this.tmService.teamMember$.subscribe(data => this.teamMember = data);
+    console.log(Cookie.get('user'));
+  }
+
+  getTeamMember() {
+
   }
 
 }
