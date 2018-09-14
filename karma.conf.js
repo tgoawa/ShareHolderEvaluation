b645e251283a5f230a9ce394d78540f7fadcdcfa
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-09-14 10:38:02 -0500

    test chromeleadless in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -22,12 +22,23 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
